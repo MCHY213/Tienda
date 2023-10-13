23fc985a25414e4c240341cc81c10c67d5fcc553
mario <mario@Mario>
2023-10-13 14:15:26 -0600

    Semana 4

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.tienda.service;
 
 import com.tienda.domain.Categoria;
 import java.util.List;
 
-/**
- *
- * @author mario
- */
 public interface CategoriaService {
     
     public List<Categoria> getCategorias(boolean activo);
